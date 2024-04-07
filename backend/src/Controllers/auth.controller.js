57d903f1d84f3c0e40b5fe4777233ce3966e28d9
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-04-07 20:28:00 +0530

    separate auth route user route auth controllers user controllers and add post route post controller

--- backend/src/Controllers/auth.controller.js
+++ backend/src/Controllers/auth.controller.js
@@ -4,9 +4,10 @@ import User from "../Models/user.model.js";
 import apiResponse from "../Utils/apiResponse.js";
 import bcryptjs from 'bcryptjs'
 import dotenv from "dotenv";
-import { uploadOnCloudinary } from "../Services/cloudinar.yservices.js";
 
-dotenv.config({path:"./.env"})
+dotenv.config(
+    {path:"./.env"}
+)
 
 const generateAccessAndRefreshToken = async(userId) => {
     try{
@@ -36,7 +37,7 @@ const options = {
     secure: true
 }
 export const registerUser = asyncHandler(async (req, res, next)=>{
-    console.log(req.body)
+    // console.log(req.body)
     const {username, email, password} = req.body;
     if(
         [username, email, password].some(field=>field?.trim()?0:1)
@@ -50,7 +51,7 @@ export const registerUser = asyncHandler(async (req, res, next)=>{
         }
     )
 
-    console.log(isUserExists);
+    // console.log(isUserExists);
 
     if(isUserExists){
         throw new apiError(409, "USER with this credentials already EXISTS!");
@@ -73,7 +74,7 @@ export const registerUser = asyncHandler(async (req, res, next)=>{
 })
 
 export const loginUser = asyncHandler(async (req, res, next)=>{
-    // console.log(req.body)
+    console.log(req.body)
     const {email, password} = req.body;
     if(
         [email, password].some(field=>field?.trim()?0:1)
@@ -85,7 +86,8 @@ export const loginUser = asyncHandler(async (req, res, next)=>{
     if(!currentUser){
         throw new apiError(409, "User doesn't exist");
     }
-
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    console.log(hashedPassword, currentUser?.password);
     const passwordValidation = bcryptjs.compareSync(password, currentUser?.password);
     if(!passwordValidation){
         throw new apiError(409, "Credentials didn't match.");
@@ -114,84 +116,7 @@ export const loginUser = asyncHandler(async (req, res, next)=>{
  * cloudianry.upload.upload
  * upload and get reponse also unlink file * 
  */
-export const uploadProfilePicture = asyncHandler(async(req, res, next)=>{
-    console.log(req.file.path);
-    const profileLocalPath = req.file.path;
-    if(!profileLocalPath){
-        throw new apiError(400, "please select an image!")
-    }
-    try {
-        const response = await uploadOnCloudinary(profileLocalPath);
-        if(!response){
-            throw new apiError(500, "error uploading profile picutre");
-        }
-        console.log(response);  
-        const currentUser = await User.findById(req.user?._id).select("-password -refreshToken");
-        if(!currentUser){
-            throw new apiError(500, "Failed to upload profile image.")
-        }     
-        // currentUser.photoURL = response.url;
-        // await currentUser
-        //     .save()
-        //     .then(savedUser=>console.log(savedUser))
-        //     .catch(error=>console.log("failed to update database", error))
-        // console.log('updated user', updatedUser);
-        //Or
 
-        const updatedUser = await User.findByIdAndUpdate(
-            req.user?._id,
-            {
-                $set:{
-                    photoURL: response.url,
-                },
-            },            
-            {
-                new: true
-            }
-        ).select("-password -refreshToken");
-        if(!updatedUser){
-            throw new apiError(500, "failed to update data base");
-        }
-        console.log(updatedUser);
-        return res
-            .status(200)
-            .json(
-                new apiResponse(200, "profile udpate SUCCESS", updatedUser)
-            )
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-export const updateAccoutDetails = asyncHandler(async (req, res, next)=>{
-    const {username, email, password} = req.body;
-    if(
-        [username, email, password].some(field=>field?.trim()?0:1)
-    ){
-        throw new apiError(409, "All fields are required");
-    }
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const currentUser = await User.findByIdAndUpdate(
-        req.user?._id, {
-            $set: {
-                username,
-                email,
-                password: hashedPassword
-            },
-        },
-        {
-            new: true
-        }
-    ).select("-password -refreshToken");
-    if(!currentUser){
-        throw new apiError(500, "FAILED to update user!")
-    }
-    return res  
-            .status(200)
-            .json(
-                new apiResponse(200, "User update SUCCESS!", currentUser,)
-            )
-})
 
 export const deleteUser = asyncHandler(async (req, res, next)=>{
     try {
@@ -207,4 +132,30 @@ export const deleteUser = asyncHandler(async (req, res, next)=>{
         console.log(error)
     }
     
+})
+
+export const logoutUser = asyncHandler(async (req, res, next)=>{
+    const updatedUser = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                refreshToken: 1,
+            }
+        },
+        {
+            new: true,
+        }
+    );
+    if(!updatedUser){
+        throw new apiError(500, "Unable si logout User!");
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(
+            new apiResponse(200, "User Logged out", null)
+        )
+
 })
---
+++ backend/src/Controllers/user.controller.js
@@ -0,0 +1,85 @@
+import asyncHandler from "../Utils/asyncHandler.js"
+import apiError from "../Utils/apiError.js";
+import User from "../Models/user.model.js";
+import apiResponse from "../Utils/apiResponse.js";
+import bcryptjs from 'bcryptjs'
+import { uploadOnCloudinary } from "../Services/cloudinar.yservices.js";
+
+export const uploadProfilePicture = asyncHandler(async(req, res, next)=>{
+    console.log(req.file.path);
+    const profileLocalPath = req.file.path;
+    if(!profileLocalPath){
+        throw new apiError(400, "please select an image!")
+    }
+    try {
+        const response = await uploadOnCloudinary(profileLocalPath);
+        if(!response){
+            throw new apiError(500, "error uploading profile picutre");
+        }
+        console.log(response);  
+        const currentUser = await User.findById(req.user?._id).select("-password -refreshToken");
+        if(!currentUser){
+            throw new apiError(500, "Failed to upload profile image.")
+        }     
+        // currentUser.photoURL = response.url;
+        // await currentUser
+        //     .save()
+        //     .then(savedUser=>console.log(savedUser))
+        //     .catch(error=>console.log("failed to update database", error))
+        // console.log('updated user', updatedUser);
+        //Or
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                $set:{
+                    photoURL: response.url,
+                },
+            },            
+            {
+                new: true
+            }
+        ).select("-password -refreshToken");
+        if(!updatedUser){
+            throw new apiError(500, "failed to update data base");
+        }
+        console.log(updatedUser);
+        return res
+            .status(200)
+            .json(
+                new apiResponse(200, "profile udpate SUCCESS", updatedUser)
+            )
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+export const updateAccoutDetails = asyncHandler(async (req, res, next)=>{
+    const {username, email, password} = req.body;
+    if(
+        [username, email, password].some(field=>field?.trim()?0:1)
+    ){
+        throw new apiError(409, "All fields are required");
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const currentUser = await User.findByIdAndUpdate(
+        req.user?._id, {
+            $set: {
+                username,
+                email,
+                password: hashedPassword
+            },
+        },
+        {
+            new: true
+        }
+    ).select("-password -refreshToken");
+    if(!currentUser){
+        throw new apiError(500, "FAILED to update user!")
+    }
+    return res  
+            .status(200)
+            .json(
+                new apiResponse(200, "User update SUCCESS!", currentUser,)
+            )
+})
