d5c4ca11205aa370f4a54a333577cd7bdf7248ec
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-04-08 16:11:45 +0530

    add delete user routes and controllers

--- backend/src/Controllers/comment.controller.js
+++ backend/src/Controllers/comment.controller.js
@@ -34,3 +34,47 @@ export const createComment = asyncHandler(async (req, res, next)=>{
         console.log(error);
     }
 })
+
+export const commentsOnPost = asyncHandler(async (req, res, next)=>{
+    const postId = req.params?.postId;
+    try {
+        const post = await Post.findById(postId);
+        if(!post){
+            throw new apiError(409, "no post found")
+        }
+        const comments = await Comment.find({post});
+        if(!comments){
+            throw new apiError(500, "no comments yet")
+        }
+        const commentsCount = comments.length;
+        return res
+            .status(200)
+            .json(
+                new apiResponse(200, "comments fetched", {comments, commentsCount})
+            );
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+export const deleteComment = asyncHandler(async (req, res, next)=>{
+    console.log(req.params);
+    if(req.user?._id != req.params?.userId){
+        throw new apiError(409, "You can delete only your comments")
+    }
+    try {
+
+        const deletedComment = await Comment.findByIdAndDelete(req.params?.commentId);
+        console.log(deletedComment);
+        if(!deletedComment){
+            throw new apiError(409, "error deleting comment")
+        }
+        return res  
+            .status(200)
+            .json(
+                new apiResponse(200, "comment deleted", deleteComment)
+            );
+    } catch (error) {
+        console.log(error)
+    }
+})
--- backend/src/Routes/comment.route.js
+++ backend/src/Routes/comment.route.js
@@ -1,5 +1,5 @@
 import Router from 'express';
-import { createComment } from '../Controllers/comment.controller.js';
+import { commentsOnPost, createComment, deleteComment } from '../Controllers/comment.controller.js';
 import { verifyUser } from '../Middlewares/auth.middleware.js';
 import upload from '../Middlewares/multer.middleware.js'
 
@@ -7,5 +7,9 @@ const router = Router();
 
 
 router.route('/create-comment').post(upload.none(),  verifyUser, createComment);
+router.route('/get-post-comment/:postId').get(commentsOnPost)
+router.route('/delete-comment/:commentId/:userId').delete(verifyUser, deleteComment);
+
+
 
 export default router;
