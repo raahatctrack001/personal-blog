a0cb609c3fa30ed379d722ae846e91399be8b4eb
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-04-13 21:13:40 +0530

    add header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path = "/" element = { <Home /> } />
-        <Route path = "/sign-in" element = { <SignIn /> } />
-        <Route path = "/sign-up" element = { <SignUp /> } />
-        <Route path= "/about" element = { <About /> } />
-        <Route path= "/dashboard" element = { <Dashboard /> } />
-        <Route path= "/projects" element = {<Projects />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/project' element = {<Projects />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/dashboard' element = {<Dashboard />} />
+        <Route path='sign-in' element = {<SignIn />} />
+        <Route path='sign-up' element = {<SignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import React from 'react'
+import { AiOutlineInsertRowAbove, AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { Link, useLocation } from 'react-router-dom';
+
+
+
+const header = () => {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className='border-b-2 bg-gray-200 text-gray-800 md:text-lg font-semibold'>
+      <div className='bg-white shadow-2xl p-1 rounded-lg'> <span className='self-center whitespace-nowrap'> Raahat's </span> Blog </div>
+    
+    <form>
+      <TextInput 
+        placeholder='search...'
+        rightIcon= {AiOutlineSearch}
+        className='hidden md:inline'
+      />
+    </form>
+    <Button outline className='h-10 bg-green-500 md:hidden'> <AiOutlineSearch /> </Button>
+    <div className='flex gap-2 md:order-2'>
+      <Button outline className='w-14 h-10 bg-green-500'> <FaMoon/> </Button>
+      <Button outline className='h-10 bg-green-500 text-green-800'> Sign In </Button>
+      <Navbar.Toggle />
+    </div>
+
+    <Navbar.Collapse>
+      <Navbar.Link as={'div'} active = {path === '/'}>
+        <Link to={'/'}>
+          Home
+        </Link>
+      </Navbar.Link>
+
+      <Navbar.Link as={'div'} active = {path === '/about'}>
+        <Link to={'/about'}>
+          About
+        </Link>
+      </Navbar.Link>
+      
+      <Navbar.Link as={'div'} active = {path === '/project'}>
+        <Link to={'/project'}>
+          Projects
+        </Link>
+      </Navbar.Link>
+      
+    </Navbar.Collapse>
+    
+    
+    </Navbar>
+  )
+}
+
+export default header
